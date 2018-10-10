31f513cde02b9902fcef793280561210dab34562
Adam Świderski <adam.swiderski89@gmail.com>
2018-10-10 08:26:45 +0200

    added some solutions

--- src/main/kotlin/com/vanniktech/rxriddles/Riddle4.kt
+++ src/main/kotlin/com/vanniktech/rxriddles/Riddle4.kt
@@ -1,15 +1,16 @@
 package com.vanniktech.rxriddles
 
 import io.reactivex.Observable
+import io.reactivex.functions.BiFunction
 
 object Riddle4 {
-  /**
-   * Implement a toggle mechanism. Initially we want to return false.
-   * Every time [source] emits, we want to negate the previous value.
-   *
-   * Use case: Some button that can toggle two states. For instance a switch between White & Dark theme.
-   */
-  fun solve(source: Observable<Unit>): Observable<Boolean> {
-    TODO()
-  }
+    /**
+     * Implement a toggle mechanism. Initially we want to return false.
+     * Every time [source] emits, we want to negate the previous value.
+     *
+     * Use case: Some button that can toggle two states. For instance a switch between White & Dark theme.
+     */
+    fun solve(source: Observable<Unit>): Observable<Boolean> {
+        return source.scan(false, { previous, _ -> !previous })
+    }
 }
